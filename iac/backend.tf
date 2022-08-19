4670ad3b2ebc617d9cd47847e9811bd2e86e6bbb
Fausto Correa <fcorrea@architech.ca>
2022-08-19 14:57:42 -0400

    ABC

--- iac/backend.tf
+++ iac/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     # modify the values for the storage account
     storage_account_name = "sagafcorq"
-    container_name       = "testingterraform"
+    container_name       = "testterraform"
     key                  = "terraform.tfstate"
   }
 }
